fcd5de3a3312390a5ef99860f52042939061b6ec
Steven Kang <stevenkang3@gmail.com>
2015-09-28 17:05:43 -0700

    updated server.js to include sockets

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 
 var app = express();
 
+io = require('socket.io').listen(server)
+
 var bodyParser = require('body-parser')
 app.use(bodyParser.json());
 
@@ -12,6 +14,7 @@ require('./server/config/mongoose.js');
 
 require('./server/config/routes.js')(app);
 
+require('./server/config/socketRoutes.js')(io);
 
 app.listen(8000, function(){
 	console.log('Connected to port 8000');
